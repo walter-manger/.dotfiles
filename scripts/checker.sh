200bc85dd8f2a5552f60c7af03a21bc1b50e3832
walter-manger <walter.manger@gmail.com>
2022-02-11 14:54:48 -0700

    adding some scripts

--- scripts/checker.sh
+++ scripts/checker.sh
@@ -1,3 +1,31 @@
 #!/usr/bin/env bash
 
-echo placeholder
+#echo placeholder
+
+# check to see if there are any changes in personal
+check_personal() {
+    for d in ~/repos/walter-manger/* ; do
+        if [ -d "$d/.git" ]
+        then
+            changes=$(git -C $d status -s | wc -l 2>&1 | tr -d ' ')
+            if [ "$changes" -ne "0" ]
+            then
+                is_remote=$(git ls-remote --heads --exit-code origin "$(git symbolic-ref --short HEAD)")
+                if [[ -z $is_remote ]]
+                then
+                    echo "$d has $changes changes, but does not exist remotely"
+                else
+                    echo "$d has $changes changes and exists remotely"
+                fi
+            fi
+
+        fi
+    done
+}
+
+check_ssh_config() {
+    awk '/IdentityFile/ {print $2}' ~/.ssh/config | while IFS= read -r line; do echo "checking $line"; if [ -f "$line" ]; then echo "file $line exists!"; fi; done
+}
+
+#check_personal;
+check_ssh_config;
